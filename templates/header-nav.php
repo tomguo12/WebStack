2a2292d4ab5e26942ab14b5e85ad3ccb85b0521c
tomguo12 <112546072+tomguo12@users.noreply.github.com>
2024-04-23 11:04:59 +0800

    Update header-nav.php

--- templates/header-nav.php
+++ templates/header-nav.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * @Theme Name:WebStack
- * @Theme URI:https://www.iotheme.cn/
- * @Author: iowen
- * @Author URI: https://www.iowen.cn/
+ * @Theme URI:https://56world.pp.ua
+ * @Author: Midou
+ * @Author URI: https://56world.pp.ua
  * @Date: 2019-02-22 21:26:02
- * @LastEditors: iowen
+ * @LastEditors: Midou
  * @LastEditTime: 2023-02-20 19:59:51
  * @FilePath: \WebStack\templates\header-nav.php
  * @Description: 
